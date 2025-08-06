1904c96f8d8713ea5fab5ec24ce44dbc0b109b66
Richmann <richmanaddai@gmail.com>
2025-08-06 00:24:52 +0000

    New Portfolio 2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  
+  devToolbar: {
+    enabled: false
+  },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   }
 });
